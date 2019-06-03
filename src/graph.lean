bef38902510e34ec2422523720e81ce0bb8f0a3d
Gun Pinyo <gunpinyo@gmail.com>
2019-06-03 18:55:42 +0100

    continue working on graph morphisms

--- src/graph.lean
+++ src/graph.lean
@@ -1,6 +1,9 @@
 import utils
 import data.vector2
 
+-- set_option pp.beta true
+attribute [reducible] vector.cons
+
 structure graph :=
   (nodes  : Type _)
   (edges  : Type _)
@@ -55,44 +58,120 @@ def standard_prism_graph : graph → graph := prism_graph_aux ff
 def twisted_prism_graph  : graph → graph := prism_graph_aux tt
 
 def standard_cube_graph : ℕ → graph
-| 0     := singleton_reflexive_graph
-| (n+1) := standard_prism_graph (standard_cube_graph n)
+| nat.zero     := singleton_reflexive_graph
+| (nat.succ n) := standard_prism_graph (standard_cube_graph n)
 def twisted_cube_graph : ℕ → graph
 | nat.zero     := singleton_reflexive_graph
 | (nat.succ n) := twisted_prism_graph (twisted_cube_graph n)
 
+
+@[simp]
 def standard_cube_graph_alt (n : ℕ) : graph :=
   { nodes  := bitvec n
-  , edges  := bitvec n + (match n with
-                          | nat.zero      := empty
-                          | (nat.succ n') := fin n × bitvec n'
-                          end : Type _)
+  , edges  := bitvec n + (fin n × match n with
+                                  | nat.zero      := empty
+                                  | (nat.succ n') := bitvec n'
+                                  end)
   , srctrg := λ b e, match n, e with
                      | _,            sum.inl v      := v
-                     | (nat.succ _), sum.inr (i, v) := v.insert_nth b i
+                     | (nat.succ _), sum.inr (i, v) := vector.insert_nth b i v
                      end
   }
 
-def standard_cube_graph_to_alt :
-      Π (n : ℕ), graph.hom (standard_cube_graph n) (standard_cube_graph_alt n)
-| nat.zero     := { nodes_map  := λ _, vector.nil
-                  , edges_map  := λ _, sum.inl vector.nil
-                  , srctrg_map := λ b e, rfl
-                  }
-| (nat.succ n) := let ih_hom := standard_cube_graph_to_alt n in
-                  { nodes_map  := λ ⟨b, v⟩, (ih_hom.nodes_map v).cons b
-                  , edges_map  := λ e',
-    match e' with
-    | sum.inl ⟨b, e⟩ :=
-        match n, ih_hom.edges_map e with
-        | _,            sum.inl v      := sum.inl (b :: v)
-        | (nat.succ _), sum.inr (i, v) := sum.inr ⟨fin.succ i, b :: v⟩
-        end
-    | sum.inr v      := sum.inr (⟨0, nat.zero_lt_succ n⟩, ih_hom.nodes_map v)
-    end
-                  , srctrg_map := λ b' e',
-    match e' with
-    | sum.inl ⟨b, e⟩ := {!!}
-    | sum.inr v      := {!!}
-    end
-                  }
+namespace standard_cube_graph_to_alt
+  def scg  := standard_cube_graph
+  def scg' := standard_cube_graph_alt
+
+  @[simp]
+  def nodes_map : Π (n : ℕ), (scg n).nodes → (scg' n).nodes
+  | nat.zero     _      := vector.nil
+  | (nat.succ n) ⟨b, v⟩ := (nodes_map n v).my_cons b
+
+  @[simp]
+  def edges_map : Π (n : ℕ), (scg n).edges → (scg' n).edges
+  | nat.zero     _                := sum.inl vector.nil
+  | (nat.succ n) (sum.inl ⟨b, e⟩) :=
+      match n, edges_map n e with
+      | _,            sum.inl v      := sum.inl (v.my_cons b)
+      | (nat.succ _), sum.inr (i, v) := sum.inr ⟨fin.succ i, v.my_cons b⟩
+      end
+  | (nat.succ n) (sum.inr v)      := sum.inr (fin.zero, nodes_map n v)
+
+  @[simp]
+  def srctrg_map : Π (n : ℕ) (b : bool) (e : (scg n).edges),
+    nodes_map n ((scg n).srctrg b e) = (scg' n).srctrg b (edges_map n e)
+  | nat.zero     _ _                 := rfl
+  | (nat.succ n) b (sum.inl ⟨ff, e⟩) := {! srctrg_map n b e !}
+  | (nat.succ n) b (sum.inl ⟨tt, e⟩) :=
+      (sorry : nodes_map (nat.succ n) (tt, (scg n).srctrg b e)
+         = (scg' (nat.succ n)).srctrg b (edges_map (nat.succ n) (sum.inl (tt, e))))
+  | (nat.succ n) b  (sum.inr v)       := rfl
+
+variable (n : ℕ)
+variables (b b' : bool)
+variable (e : (standard_cube_graph n).edges)
+#reduce nodes_map (nat.succ n) ((scg (nat.succ n)).srctrg b (sum.inl (b', e)))
+#reduce (scg' (nat.succ n)).srctrg b (edges_map (nat.succ n) (sum.inl (b', e)))
+
+
+
+  def main (n : ℕ) : graph.hom (scg n) (scg n)
+  { nodes_map  := nodes_map n
+  , edges_map  := edges_map n
+  , srctrg_map := λ b' e',
+      match e' with
+      | sum.inl ⟨b, e⟩ := _
+          -- match n, ih_hom.edges_map e with
+          -- | _,            sum.inl v      := _
+          -- | (nat.succ _), sum.inr (i, v) := _
+          -- end
+      | sum.inr v      := rfl
+      end
+                    }
+
+end standard_cube_graph_to_alt
+
+-- #check Π (n : ℕ), (graph.hom (standard_cube_graph n) (standard_cube_graph_alt n)).nodes_map
+-- #check graph.hom.nodes_map
+-- @[simp]
+-- def standard_cube_graph_to_alt :
+--       Π (n : ℕ), graph.hom (standard_cube_graph n) (standard_cube_graph_alt n)
+-- | nat.zero     := { nodes_map  := _ -- λ _, vector.nil
+--                   , edges_map  := λ _, sum.inl vector.nil
+--                   , srctrg_map := λ b e, rfl
+--                   }
+-- | (nat.succ n) := let ih_hom := standard_cube_graph_to_alt n in
+--                   { nodes_map  := λ ⟨b, v⟩, (ih_hom.nodes_map v).my_cons b
+--                   , edges_map  := λ e',
+--     match e' with
+--     | sum.inl ⟨b, e⟩ :=
+--         match n, ih_hom.edges_map e with
+--         | _,            sum.inl v      := sum.inl (v.my_cons b)
+--         | (nat.succ _), sum.inr (i, v) := sum.inr ⟨fin.succ i, v.my_cons b⟩
+--         end
+--     | sum.inr v      := sum.inr (fin.zero, ih_hom.nodes_map v)
+--     end
+--                   , srctrg_map := λ b' e',
+--     match e' with
+--     | sum.inl ⟨b, e⟩ := _
+--         -- match n, ih_hom.edges_map e with
+--         -- | _,            sum.inl v      := _
+--         -- | (nat.succ _), sum.inr (i, v) := _
+--         -- end
+--     | sum.inr v      := rfl
+--     end
+--                   }
+
+-- variable standard_cube_graph_to_alt : Π (n : ℕ), graph.hom (standard_cube_graph n) (standard_cube_graph_alt n)
+-- variable n : ℕ
+-- variable ih_hom : graph.hom (standard_cube_graph n) (standard_cube_graph_alt n)
+-- variable b' : bool
+-- variable e' : (standard_cube_graph (nat.succ n)).edges
+-- variable b : bool
+-- variable e : (standard_cube_graph n).edges
+
+-- #reduce _root_.standard_cube_graph_to_alt._match_1 n ih_hom
+--       ((standard_cube_graph (nat.succ n)).srctrg b' (sum.inl (b, e)))
+-- -- =
+-- --     (standard_cube_graph_alt (nat.succ n)).srctrg b'
+-- --       (_root_.standard_cube_graph_to_alt._match_2 n ih_hom (sum.inl (b, e)))
--- src/utils.lean
+++ src/utils.lean
@@ -1,3 +1,26 @@
 import data.bitvec
+import data.vector2
 
 notation α + β := sum α β
+
+namespace fin
+
+def zero {n : ℕ} : fin (nat.succ n) := ⟨nat.zero, nat.zero_lt_succ n⟩
+
+end fin
+
+namespace vector
+variable {n : ℕ}
+variable {α : Type _}
+
+def my_cons (a : α) (v : vector α n) : vector α (nat.succ n) :=
+  v.insert_nth a fin.zero
+
+def eq_from_list : Π {a b : vector α n}, a.val = b.val → a = b
+| ⟨l, p⟩ ⟨.(l), .(p)⟩ rfl := rfl
+
+-- def my_insert_nth (a : α) : fin (nat.succ n) → vector α n
+--                                              → vector α (nat.succ n)
+-- | fin.succ ()
+
+end vector
